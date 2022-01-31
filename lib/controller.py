bfbcf413b9ccd7a89a72d2e2abf15f3f0f54956d
frozaidi <nmfarhan@gmail.com>
2022-01-31 14:02:58 -0800

    implemented concurrent line following with RossRos module

--- lib/controller.py
+++ lib/controller.py
@@ -5,12 +5,13 @@ from sensor import GrayscaleSensor
 
 
 class Controller(object):
-    def __init__(self, scale=1):
+    def __init__(self, scale=1, px):
         # Set a scale to the [-1,1] range of direction given by the interpreter
         # which matches the maximum angle of the PicarX robot
         self.scale = scale*40
+        self.px = px
 
-    def line_follow(self, px, rel_dir):
+    def line_follow(self, rel_dir):
         """
         Function to set the steering angle based on the direction and scale
         :param px: The Picarx object, used to command the steering angle
@@ -18,11 +19,11 @@ class Controller(object):
         positive values indicate the line left of the robot
         """
         self.steer_angle = -1*rel_dir*self.scale
-        px.set_dir_servo_angle(self.steer_angle)
+        self.px.set_dir_servo_angle(self.steer_angle)
         # Return the steer angle as feedback
         return self.steer_angle
 
-    def cont_bus(self, px, bus, delay):
+    def cont_bus(self, bus, delay):
         """
         Function to run the line follow function in a loop using the interpret
         bus to read direction value messages for each loop and change the steer
@@ -33,7 +34,7 @@ class Controller(object):
         """
         while True:
             rel_dir = bus.read()
-            self.line_follow(px, rel_dir)
+            self.line_follow(rel_dir)
             time.sleep(delay)
 
 
---
+++ scripts/rossros_line_follow.py
@@ -0,0 +1,51 @@
+import concurrent.futures
+import sys
+sys.path.append(r'/home/frozaidi/picar-x/lib')
+sys.path.append(r'/home/frozaidi/picar-x/rossros')
+import rossros as rr                    # noqa: E402
+from picarx_improved import Picarx      # noqa: E402
+from interpreter import Interpreter     # noqa: E402
+from sensor import GrayscaleSensor      # noqa: E402
+from controller import Controller       # noqa: E402
+
+if __name__ == '__main__':
+    px = Picarx()
+    scale = int(input("Enter scale: "))
+    polarity = int(input("Enter polarity:"))
+    con = Controller(scale)
+    sens = GrayscaleSensor()
+    inter = Interpreter(0.0, polarity)
+    sens_bus = rr.Bus(0, "Grayscale Sensor Bus")
+    inter_bus = rr.Bus(0, "Grayscale Interpreter Bus")
+    term_bus = rr.Bus(0, "Termination Bus")
+
+    sens_delay = 0.01
+    inter_delay = 0.01
+    cont_delay = 0.01
+    term_delay = 0.01
+
+    gry_sens = rr.Producer(sens.get_grayscale_data, sens_bus, sens_delay,
+                           term_bus, "Grayscale Producer")
+
+    gry_inter = rr.ConsumerProducer(inter.edge_detect, sens_bus, inter_bus,
+                                    inter_delay, term_bus,
+                                    "Grayscale Interpreter")
+
+    gry_cont = rr.Consumer(con.line_follow, inter_bus, cont_delay, term_bus,
+                           "Grayscale Controller")
+
+    term_timer = rr.Timer(term_bus, 5, term_delay, term_bus,
+                          "Termination Timer")
+
+    px.forward(40)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        eGrySensor = executor.submit(gry_sens)
+        eGryInterp = executor.submit(gry_inter)
+        eGryCont = executor.submit(gry_cont)
+        eTimer = executor.submit(term_timer)
+
+    eGrySensor.result()
+    eGryInterp.result()
+    eGryCont.result()
+    eTimer.result()
